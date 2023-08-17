b3e7f9a8b3400a0789f13a122718bd2e290e2fd4
Anna <madam.sestra@gmail.com>
2023-08-17 18:41:20 +0300

    ex1

--- src/main/java/home/test/Ex1.java
+++ src/main/java/home/test/Ex1.java
@@ -3,71 +3,76 @@ package home.test;
 public class Ex1 {
 
     public FuncResult mainFunc(int[] inputArray) {
-        int max = 0;
-        int min = 0;
-        int minValue = inputArray[0];
-        int maxValue = inputArray[0];
-        int[] ar = new int[4];
-        for (int i = 0; i < inputArray.length; i++) {
-            if (inputArray[i] > max) {
-                max = inputArray[i];
-            }
-            if (inputArray[i] < min) {
-                min = inputArray[i];
-            }
-            if (inputArray[i] == min) {
-                ar[1] = i;
-            }
-            if (inputArray[i] == max) {
-                ar[3] = i;
-            }
-            if (i != 0 && inputArray[i] < minValue) {
-                minValue = inputArray[i];
-                ar[0] = i;
-            }
-            if (i != 0 && inputArray[i] > maxValue) {
-                maxValue = inputArray[i];
-                ar[2] = i;
-            }
-        }
-        int theBiggestSum = 0;
+        int total = 0;
         int first = 0;
         int last = 0;
-        int firstIndex = 0;
-        int lastIndex = 0;
-        for (int i = 0; i < 2; i++) {
-            int a = ar[i];
-            int b = ar[i + 2];
-            int c = ar[3 - i];
+        if (inputArray.length == 0) {
+        } else if (inputArray.length > 0) {
+            int max = inputArray[0];
+            int min = inputArray[0];
+            int minValue = inputArray[0];
+            int maxValue = inputArray[0];
+            int[] ar = new int[4];
 
-            if (Math.abs(a - b) >= theBiggestSum) {
-                theBiggestSum = a + b;
-                if (a < b) {
-                    firstIndex = a;
-                    lastIndex = b;
-                } else if (a > b) {
-                    firstIndex = b;
-                    lastIndex = a;
+            for (int i = 0; i < inputArray.length; i++) {
+                if (inputArray[i] > max) {
+                    max = inputArray[i];
                 }
-                first = a;
-                last = b;
-            }
-            if (Math.abs(a - c) >= theBiggestSum) {
-                theBiggestSum = a + c;
-                if (a < c) {
-                    firstIndex = a;
-                    lastIndex = c;
-                } else if (a > c) {
-                    firstIndex = c;
-                    lastIndex = a;
+                if (inputArray[i] < min) {
+                    min = inputArray[i];
                 }
-                first = a;
-                last = c;
+                if (inputArray[i] == min) {
+                    ar[1] = i;
+                }
+                if (inputArray[i] == max) {
+                    ar[3] = i;
+                }
+                if (i != 0 && inputArray[i] < minValue) {
+                    minValue = inputArray[i];
+                    ar[0] = i;
+                }
+                if (i != 0 && inputArray[i] > maxValue) {
+                    maxValue = inputArray[i];
+                    ar[2] = i;
+                }
             }
-        }
-        int total = 0;
-        for (int i = firstIndex + 1; i < lastIndex; i++) {
-            total += inputArray[i];
+            int theBiggestSum = 0;
+            int firstIndex = 0;
+            int lastIndex = 0;
+            for (int i = 0; i < 2; i++) {
+                int a = ar[i];
+                int b = ar[i + 2];
+                int c = ar[3 - i];
+
+                if (Math.abs(a - b) >= theBiggestSum) {
+                    theBiggestSum = a + b;
+                    if (a < b) {
+                        firstIndex = a;
+                        lastIndex = b;
+                    } else if (a > b) {
+                        firstIndex = b;
+                        lastIndex = a;
+                    }
+                    first = a;
+                    last = b;
+                }
+                if (Math.abs(a - c) >= theBiggestSum) {
+                    theBiggestSum = a + c;
+                    if (a < c) {
+                        firstIndex = a;
+                        lastIndex = c;
+                    } else if (a > c) {
+                        firstIndex = c;
+                        lastIndex = a;
+                    }
+                    first = a;
+                    last = c;
+                }
+            }
+
+            for (int i = firstIndex + 1; i < lastIndex; i++) {
+                total += inputArray[i];
+            }
         }
         return new FuncResult(total, first, last);
     }
